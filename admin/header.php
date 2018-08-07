00ad1acbbcfef8171b34ffc158ef287f04c1f6c4
niranjan <nsadhu22@gmail.com>
2018-08-07 13:45:07 +0530

    final updated header.php

--- admin/header.php
+++ admin/header.php
@@ -8,18 +8,8 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
                     <a class="navbar-brand" href="index.html">SB admin</a>
-                </div>
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.html">SB admin</a>
-                </div>
+                </div> 
                 <!-- Top Menu Items -->
                 <ul class="nav navbar-right top-nav">
                     <li class="dropdown">
