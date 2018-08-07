ff71b550953a1ecc64e4ca402cfd52286d3b7582
niranjan <nsadhu22@gmail.com>
2018-08-07 13:34:58 +0530

    header.php changed again

--- admin/header.php
+++ admin/header.php
@@ -8,7 +8,16 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html">SB Not admin</a>
+                    <a class="navbar-brand" href="index.html">SB admin</a>
+                </div>
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html">SB admin</a>
                 </div>
                 <!-- Top Menu Items -->
                 <ul class="nav navbar-right top-nav">
